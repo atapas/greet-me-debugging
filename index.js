5f4c7fb6c513efb5a55cdb6aa656175b63f57947
Tapas Adhikary <tapas.adhikary@gmail.com>
2020-10-28 16:38:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const print = function() {
+    const name = document.getElementById('m_name').value;
+    const wish = document.getElementById('m_wish').value;
+
+    const message = 'Hello ' 
+                        + name 
+                        + ', Your wish `' 
+                        + + wish 
+                        + '` may come true!';
+    
+    document.getElementById('output').innerHTML = '<span class="message">' + message + '</span>';
+}
